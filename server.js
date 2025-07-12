bba3717ac895c237267d2d695df5bb928c5d5c43
김현성 <sseonghs@gmail.com>
2025-07-12 11:36:04 +0900

    첫 커밋

---
+++ server.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const fs = require('fs');
+const dayjs = require('dayjs');
+const userInfos = {}; 
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+const roomMessages = {}; 
+let waitingUser = null;  
+let roomIdCounter = 1;
+
+function saveChatLog(roomId) {
+    if (!roomId) return;
+    const logs = roomMessages[roomId];
+    if (!logs) return;
+
+    const participants = [...new Set(logs.map(log => `${log.username}(${log.ip})`))];
+    const filename = `${dayjs().format('YYYY-MM-DD_HH-mm')}_${participants.join('-')}.txt`;
+
+    const content = logs.map(log =>
+        `${log.timestamp} ${log.username}(${log.email},${log.ip}): ${log.message}`
+    ).join('\n');
+
+    if (!fs.existsSync('chat_logs')) {
+        fs.mkdirSync('chat_logs');
+    }
+
+    fs.writeFileSync(`chat_logs/${filename}`, content);
+    console.log(`${filename} saved`);
+    delete roomMessages[roomId];
+}
+
+
+io.on('connection', (socket) => {
+
+    socket.on('register', ({ username, major, email }) => {
+        const user = { socket, username, major, email};
+        userInfos[socket.id] = { username, ip: socket.handshake.address , email};
+        
+        if (waitingUser) {
+            const roomId = 'room-' + roomIdCounter++;
+            socket.join(roomId);
+            waitingUser.socket.join(roomId);
+            roomMessages[roomId] = [];
+
+            console.log(`matched ${waitingUser.username} (${waitingUser.major}) <-> ${username} (${major})`);
+
+            socket.emit('matched', {
+                roomId,
+                partner: {
+                    username: waitingUser.username,
+                    major: waitingUser.major
+                }
+            });
+
+            waitingUser.socket.emit('matched', {
+                roomId,
+                partner: {
+                    username,
+                    major
+                }
+            });
+
+            waitingUser = null;
+        } else {
+            waitingUser = user;
+            socket.emit('waiting');
+        }
+    });
+
+    socket.on('message', ({ roomId, message }) => {
+        const user = userInfos[socket.id] || { username: socket.id, ip: '-', email: '-' };
+        const timestamp = dayjs().format('YYYY-MM-DD HH:mm:ss');
+        const log = { timestamp, username: user.username, ip: user.ip, email:user.email, message };
+        roomMessages[roomId]?.push(log);
+        socket.to(roomId).emit('message', {username :user.username, message});
+    });
+    
+
+    socket.on('disconnecting', () => {
+        const username = userInfos[socket.id] || socket.id;
+
+        const roomId = [...socket.rooms].find(r => r !== socket.id);
+        if (roomId) {
+            socket.to(roomId).emit('partner_left');
+            saveChatLog(roomId);  
+        }
+    });
+
+    socket.on('disconnect', () => {
+    });
+        if (waitingUser && waitingUser.socket.id === socket.id) {
+            waitingUser = null;
+        }
+        delete userInfos[socket.id];
+    });
+
+  
+server.listen(PORT, '0.0.0.0' , () => {
+    console.log(`server running: http://localhost:${PORT}`);
+});
